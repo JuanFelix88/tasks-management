b8dca0c68b58c483b03566b0ea3f33b4ca51daa4
Juan Andrade <andrade.juan@outlook.com.br>
2023-05-21 14:46:11 -0300

    chore: improve schedule file download layout

---
+++ mail copy.js
@@ -0,0 +1,166 @@
+const mail = `To: 
+Cc: 
+Subject: 
+X-Unsent: 1
+Content-Type: multipart/mixed; boundary=--boundary_text_string
+
+----boundary_text_string
+Content-Type: text/html; charset=UTF-8
+
+<body lang=PT-BR link="#0563C1" vlink="#954F72" style='tab-interval:35.4pt'>
+
+<div class=WordSection1>
+<p class=MsoNormal>
+<span style='mso-ascii-font-family:Calibri;mso-fareast-font-family:
+"Times New Roman";mso-hansi-font-family:Calibri;mso-bidi-font-family:Calibri;
+color:#1F497D;mso-fareast-language:PT-BR'>
+Bom dia, boa tarde e boa noite.
+</span></p>
+
+
+<p class=MsoNormal><span style='mso-ascii-font-family:Calibri;mso-fareast-font-family:
+"Times New Roman";mso-hansi-font-family:Calibri;mso-bidi-font-family:Calibri;
+color:#1F497D;mso-fareast-language:PT-BR'>Segue novo agendamento realizado:</span><span
+style='mso-ascii-font-family:Calibri;mso-fareast-font-family:"Times New Roman";
+mso-hansi-font-family:Calibri;mso-bidi-font-family:Calibri;color:black;
+mso-fareast-language:PT-BR'><o:p></o:p></span></p>
+
+<table class=MsoNormalTable border=0 cellspacing=0 cellpadding=0
+ style='background:#F2F2F2;border-collapse:collapse;mso-yfti-tbllook:1184;
+ mso-padding-alt:0cm 0cm 0cm 0cm'>
+ <tr style='mso-yfti-irow:0;mso-yfti-firstrow:yes;mso-yfti-lastrow:yes;
+  height:59.9pt'>
+  <td width=519 valign=top style='width:389.45pt;border:solid #0070C0 1.0pt;
+  padding:0cm 5.4pt 0cm 5.4pt;height:59.9pt'>
+  <p class=MsoNormal style='text-indent:35.4pt;background:#F2F2F2'><span
+  style='mso-ascii-font-family:Calibri;mso-fareast-font-family:"Times New Roman";
+  mso-hansi-font-family:Calibri;mso-bidi-font-family:Calibri;color:#1F497D;
+  mso-fareast-language:PT-BR'>&nbsp;</span><span style='mso-ascii-font-family:
+  Calibri;mso-fareast-font-family:"Times New Roman";mso-hansi-font-family:Calibri;
+  mso-bidi-font-family:Calibri;mso-fareast-language:PT-BR'><o:p></o:p></span></p>
+  <p class=MsoNormal style='text-indent:35.4pt;background:#F2F2F2'><span
+  style='mso-ascii-font-family:Calibri;mso-fareast-font-family:"Times New Roman";
+  mso-hansi-font-family:Calibri;mso-bidi-font-family:Calibri;color:#1F497D;
+  mso-fareast-language:PT-BR'>Placa:&nbsp;</span><b><span style='mso-ascii-font-family:
+  Calibri;mso-fareast-font-family:"Times New Roman";mso-hansi-font-family:Calibri;
+  mso-bidi-font-family:Calibri;color:#0070C0;mso-fareast-language:PT-BR'>$vehicle</span></b><span
+  style='mso-ascii-font-family:Calibri;mso-fareast-font-family:"Times New Roman";
+  mso-hansi-font-family:Calibri;mso-bidi-font-family:Calibri;mso-fareast-language:
+  PT-BR'><o:p></o:p></span></p>
+  <p class=MsoNormal style='background:#F2F2F2'><span style='mso-ascii-font-family:
+  Calibri;mso-fareast-font-family:"Times New Roman";mso-hansi-font-family:Calibri;
+  mso-bidi-font-family:Calibri;color:#1F497D;mso-fareast-language:PT-BR'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Local:&nbsp;&nbsp;</span><span
+  style='mso-ascii-font-family:Calibri;mso-fareast-font-family:"Times New Roman";
+  mso-hansi-font-family:Calibri;mso-bidi-font-family:Calibri;color:#7F6000;
+  mso-fareast-language:PT-BR'>$location</span><span style='mso-ascii-font-family:
+  Calibri;mso-fareast-font-family:"Times New Roman";mso-hansi-font-family:Calibri;
+  mso-bidi-font-family:Calibri;mso-fareast-language:PT-BR'><o:p></o:p></span></p>
+  <p class=MsoNormal style='background:#F2F2F2'><span style='mso-ascii-font-family:
+  Calibri;mso-fareast-font-family:"Times New Roman";mso-hansi-font-family:Calibri;
+  mso-bidi-font-family:Calibri;color:#1F497D;mso-fareast-language:PT-BR'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Agendamento:&nbsp;</span><u><span
+  style='mso-ascii-font-family:Calibri;mso-fareast-font-family:"Times New Roman";
+  mso-hansi-font-family:Calibri;mso-bidi-font-family:Calibri;color:#7030A0;
+  mso-fareast-language:PT-BR'>$schedule</span></u><span style='mso-ascii-font-family:
+  Calibri;mso-fareast-font-family:"Times New Roman";mso-hansi-font-family:Calibri;
+  mso-bidi-font-family:Calibri;mso-fareast-language:PT-BR'><o:p></o:p></span></p>
+  <p class=MsoNormal style='text-indent:35.4pt;background:#F2F2F2'><span
+  style='mso-ascii-font-family:Calibri;mso-fareast-font-family:"Times New Roman";
+  mso-hansi-font-family:Calibri;mso-bidi-font-family:Calibri;color:#1F497D;
+  mso-fareast-language:PT-BR'>Ordem de Serviço:&nbsp;</span><u><span
+  style='mso-ascii-font-family:Calibri;mso-fareast-font-family:"Times New Roman";
+  mso-hansi-font-family:Calibri;mso-bidi-font-family:Calibri;color:#7030A0;
+  mso-fareast-language:PT-BR'>$os</span></u><span style='mso-ascii-font-family:
+  Calibri;mso-fareast-font-family:"Times New Roman";mso-hansi-font-family:Calibri;
+  mso-bidi-font-family:Calibri;mso-fareast-language:PT-BR'><o:p></o:p></span></p>
+  <p class=MsoNormal style='background:#F2F2F2'><span style='mso-ascii-font-family:
+  Calibri;mso-fareast-font-family:"Times New Roman";mso-hansi-font-family:Calibri;
+  mso-bidi-font-family:Calibri;color:#1F497D;mso-fareast-language:PT-BR'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Prestador:&nbsp;</span><i><span
+  style='mso-ascii-font-family:Calibri;mso-fareast-font-family:"Times New Roman";
+  mso-hansi-font-family:Calibri;mso-bidi-font-family:Calibri;color:#C55A11;
+  mso-fareast-language:PT-BR'>$forn_name</span></i><span style='mso-ascii-font-family:
+  Calibri;mso-fareast-font-family:"Times New Roman";mso-hansi-font-family:Calibri;
+  mso-bidi-font-family:Calibri;mso-fareast-language:PT-BR'><o:p></o:p></span></p>
+  <p class=MsoNormal style='background:#F2F2F2'><span style='mso-ascii-font-family:
+  Calibri;mso-fareast-font-family:"Times New Roman";mso-hansi-font-family:Calibri;
+  mso-bidi-font-family:Calibri;color:#1F497D;mso-fareast-language:PT-BR'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Serviço
+  (s):&nbsp;</span><i><span style='mso-ascii-font-family:Calibri;mso-fareast-font-family:
+  "Times New Roman";mso-hansi-font-family:Calibri;mso-bidi-font-family:Calibri;
+  color:#C55A11;mso-fareast-language:PT-BR'>$service</span></i><span
+  style='mso-ascii-font-family:Calibri;mso-fareast-font-family:"Times New Roman";
+  mso-hansi-font-family:Calibri;mso-bidi-font-family:Calibri;mso-fareast-language:
+  PT-BR'><o:p></o:p></span></p>
+  <p class=MsoNormal style='background:#F2F2F2'><span style='mso-ascii-font-family:
+  Calibri;mso-fareast-font-family:"Times New Roman";mso-hansi-font-family:Calibri;
+  mso-bidi-font-family:Calibri;color:#1F497D;mso-fareast-language:PT-BR'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Fornecedor:&nbsp;</span><i><span
+  style='mso-ascii-font-family:Calibri;mso-fareast-font-family:"Times New Roman";
+  mso-hansi-font-family:Calibri;mso-bidi-font-family:Calibri;color:#C55A11;
+  mso-fareast-language:PT-BR'>$forn</span></i><span style='mso-ascii-font-family:
+  Calibri;mso-fareast-font-family:"Times New Roman";mso-hansi-font-family:Calibri;
+  mso-bidi-font-family:Calibri;mso-fareast-language:PT-BR'><o:p></o:p></span></p>
+  <p class=MsoNormal><span style='mso-ascii-font-family:Calibri;mso-fareast-font-family:
+  "Times New Roman";mso-hansi-font-family:Calibri;mso-bidi-font-family:Calibri;
+  color:#1F497D;mso-fareast-language:PT-BR'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Controle
+  Interno:&nbsp;</span><span style='mso-ascii-font-family:Calibri;mso-fareast-font-family:
+  "Times New Roman";mso-hansi-font-family:Calibri;mso-bidi-font-family:Calibri;
+  color:#00B050;mso-fareast-language:PT-BR'>$os_internal</span><span
+  style='mso-ascii-font-family:Calibri;mso-fareast-font-family:"Times New Roman";
+  mso-hansi-font-family:Calibri;mso-bidi-font-family:Calibri;mso-fareast-language:
+  PT-BR'><o:p></o:p></span></p>
+  <p class=MsoNormal><span style='mso-ascii-font-family:Calibri;mso-fareast-font-family:
+  "Times New Roman";mso-hansi-font-family:Calibri;mso-bidi-font-family:Calibri;
+  color:#1F497D;mso-fareast-language:PT-BR'>&nbsp;</span><span
+  style='mso-ascii-font-family:Calibri;mso-fareast-font-family:"Times New Roman";
+  mso-hansi-font-family:Calibri;mso-bidi-font-family:Calibri;mso-fareast-language:
+  PT-BR'><o:p></o:p></span></p>
+  </td>
+ </tr>
+</table>
+
+<p class=MsoNormal><span style='mso-ascii-font-family:Calibri;mso-fareast-font-family:
+"Times New Roman";mso-hansi-font-family:Calibri;mso-bidi-font-family:Calibri;
+color:#1F497D;mso-fareast-language:PT-BR'>Lembrando que em casos de&nbsp;</span><u><span
+style='mso-ascii-font-family:Calibri;mso-fareast-font-family:"Times New Roman";
+mso-hansi-font-family:Calibri;mso-bidi-font-family:Calibri;color:#C00000;
+mso-fareast-language:PT-BR'>desistência</span></u><span style='mso-ascii-font-family:
+Calibri;mso-fareast-font-family:"Times New Roman";mso-hansi-font-family:Calibri;
+mso-bidi-font-family:Calibri;color:#1F497D;mso-fareast-language:PT-BR'>&nbsp;sem
+aviso prévio a empresa poderá&nbsp;</span><u><span style='mso-ascii-font-family:
+Calibri;mso-fareast-font-family:"Times New Roman";mso-hansi-font-family:Calibri;
+mso-bidi-font-family:Calibri;color:#C00000;mso-fareast-language:PT-BR'>cobrar
+custos de deslocamento</span></u><span style='mso-ascii-font-family:Calibri;
+mso-fareast-font-family:"Times New Roman";mso-hansi-font-family:Calibri;
+mso-bidi-font-family:Calibri;color:#1F497D;mso-fareast-language:PT-BR'>.</span><span
+style='mso-ascii-font-family:Calibri;mso-fareast-font-family:"Times New Roman";
+mso-hansi-font-family:Calibri;mso-bidi-font-family:Calibri;color:black;
+mso-fareast-language:PT-BR'><o:p></o:p></span></p>
+
+<p class=MsoNormal><span style='mso-ascii-font-family:Calibri;mso-fareast-font-family:
+"Times New Roman";mso-hansi-font-family:Calibri;mso-bidi-font-family:Calibri;
+color:#1F497D;mso-fareast-language:PT-BR'>Contamos com a colaboração de todos
+para a disponibilidade do veículo.</span><span style='mso-ascii-font-family:
+Calibri;mso-fareast-font-family:"Times New Roman";mso-hansi-font-family:Calibri;
+mso-bidi-font-family:Calibri;color:black;mso-fareast-language:PT-BR'><o:p></o:p></span></p>
+
+
+<p class=MsoNormal><span style='font-size:12.0pt;mso-ascii-font-family:Calibri;
+mso-fareast-font-family:"Times New Roman";mso-hansi-font-family:Calibri;
+mso-bidi-font-family:Calibri;color:#0070C0;background:#DEEAF6;mso-fareast-language:
+PT-BR'>Agendamento de manutenção - Rastreamento</span><span style='mso-ascii-font-family:
+Calibri;mso-fareast-font-family:"Times New Roman";mso-hansi-font-family:Calibri;
+mso-bidi-font-family:Calibri;color:black;mso-fareast-language:PT-BR'><o:p></o:p></span></p>
+
+<p class=MsoNormal><span style='mso-ascii-font-family:Calibri;mso-fareast-font-family:
+"Times New Roman";mso-hansi-font-family:Calibri;mso-bidi-font-family:Calibri;
+color:black;mso-fareast-language:PT-BR'>&nbsp;</span><a name="_MailAutoSig"><span
+style='mso-fareast-font-family:"Times New Roman";mso-fareast-theme-font:minor-fareast;
+mso-fareast-language:PT-BR;mso-no-proof:yes'><o:p></o:p></span></a></p>
+
+<span style='mso-bookmark:_MailAutoSig'></span>
+
+<p class=MsoNormal><o:p>&nbsp;</o:p></p>
+
+</div>
+
+</body>
+
+----boundary_text_string--`
